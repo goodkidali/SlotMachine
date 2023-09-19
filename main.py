d3415bf8ed58ec7a292f32c5da4156c1933fe99f
Ali Saheb <alisaheb1234@gmail.com>
2023-09-19 12:40:31 -0400

    minor changes

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ ROWS = 3
 COLS = 3
 
 symbol_count = {
-    "A": 2,
+    "7": 2,
     "B": 4,
     "C": 6,
     "D": 8
 }
 
 symbol_value = {
-    "A": 5,
+    "7": 5,
     "B": 4,
     "C": 3,
     "D": 2
